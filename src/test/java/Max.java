a36e65838db7c1054c78e4c0963ccee8b384f76f
ManojKaarthick <manojkaarthick2@gmail.com>
2024-07-11 14:19:04 +0530

    helocommit

---
+++ src/test/java/Max.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Max {
+
+    public static void main(String[] args) {
+        int [] att={1,3,56,9};
+
+
+        Arrays.sort(att);
+
+        for(int i:att){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+    }
+}
